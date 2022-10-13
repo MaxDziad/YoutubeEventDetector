8a9e67319b5632469367bf22358ed7991e52e87b
Max-Home <mdziadon@drawdistance.dev>
2022-10-13 19:22:01 +0200

    reading each video file from catalogue

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 import cv2
+import os
 from videoStateMachine import VideoStateMachine
 
 if __name__ == '__main__':
 
     # global parameters
-    video_path = "VideoSources/692219490.mp4"
     frame_reading_speed = 10
 
-    # video reading
-    video = cv2.VideoCapture(video_path)
-    ret, previous_frame = video.read()
+    # reading each video file in catalogue
+    video_catalogue_path = "VideoSources"
+    for file in os.listdir(video_catalogue_path):
+        if file.endswith(".mp4"):
+            video_path = video_catalogue_path + "/" + file
 
-    WIDTH = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    HEIGHT = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            # video reading
+            video = cv2.VideoCapture(video_path)
+            ret, previous_frame = video.read()
 
-    # initializing state machine
-    stateMachine = VideoStateMachine()
-    stateMachine.initialize(WIDTH, HEIGHT)
+            WIDTH = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+            HEIGHT = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    while video.isOpened():
-        is_read, current_frame = video.read()
+            # initializing state machine
+            stateMachine = VideoStateMachine()
+            stateMachine.initialize(WIDTH, HEIGHT)
 
-        if is_read:
-            copied_previous_frame = previous_frame.copy()
-            current_time = video.get(cv2.CAP_PROP_POS_MSEC)
-            stateMachine.run_current_state(copied_previous_frame, current_frame, current_time)
+            while video.isOpened():
+                is_read, current_frame = video.read()
 
-            # cv2.waitKey waits in milliseconds before extracting next frame
-            #if cv2.waitKey(frame_reading_speed) == ord('q'):
-                #break
+                if is_read:
+                    copied_previous_frame = previous_frame.copy()
+                    current_time = video.get(cv2.CAP_PROP_POS_MSEC)
+                    stateMachine.run_current_state(copied_previous_frame, current_frame, current_time)
 
-            previous_frame = current_frame
-        else:
-            break
+                    # cv2.waitKey waits in milliseconds before extracting next frame
+                    #if cv2.waitKey(frame_reading_speed) == ord('q'):
+                        #break
 
-    video.release()
-    cv2.destroyAllWindows()
+                    previous_frame = current_frame
+                else:
+                    break
+
+            video.release()
+            cv2.destroyAllWindows()
